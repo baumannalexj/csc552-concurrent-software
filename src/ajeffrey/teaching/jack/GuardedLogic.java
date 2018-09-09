2094ec1780c27a4209cdbb08b4c587bf8a938662
baumannalexj <baumannalexj@gmail.com>
2018-09-09 15:00:50 -0500

    removed final qualifier

--- src/ajeffrey/teaching/jack/GuardedLogic.java
+++ src/ajeffrey/teaching/jack/GuardedLogic.java
@@ -16,19 +16,29 @@ import java.io.PrintWriter;
  * @version 1.0.1
  */
 
+/**
+ *
+ * CSC 552 - fall 2018
+ * Alexander Baumann
+ * HW 1
+ *
+ * Added global should-run-flag, all running threads use same flag instead of each thread
+ * managing it's own flag.
+ *
+ * changes are marked in comments as // hw1
+ *
+ * */
 
 
 public interface GuardedLogic {
-
     public static LogicFactory factory = new GuardedLogicFactoryImpl();
-
 }
 
 class GuardedLogicFactoryImpl implements LogicFactory {
-    protected Guard globalShouldRun = Guard.factory.build(true);
+    protected Guard globalShouldRun = Guard.factory.build(true); //hw 1
 
     public Logic build() {
-        return new GuardedLogicImpl(globalShouldRun);
+        return new GuardedLogicImpl(globalShouldRun); //hw1
     }
 
 }
@@ -38,12 +48,12 @@ class GuardedLogicImpl implements Logic, Runnable {
     protected final Thread thread = new Thread(this);
     protected final String message = "\nAll work and no play makes Jack a dull boy.";
     protected PrintWriter out = DevNull.printWriter;
-    //    protected Guard flag = Guard.factory.build(true);
-    protected final Guard flag;
+    //    protected Guard flag = Guard.factory.build(true); //  hw1
+    protected Guard flag;
 
     protected int offset = 0;
 
-    public GuardedLogicImpl(Guard flag) {
+    public GuardedLogicImpl(Guard flag) { //hw 1
         this.flag = flag;
     }
 
@@ -61,7 +71,7 @@ class GuardedLogicImpl implements Logic, Runnable {
         Debug.out.println("GuardedLogic.setPrintWriter (): Returning");
     }
 
-    public void suspend() {
+    public synchronized void suspend() {
         Debug.out.println("GuardedLogic.suspend (): Starting");
         flag.setValue(false);
         Debug.out.println("GuardedLogic.suspend (): Returning");
