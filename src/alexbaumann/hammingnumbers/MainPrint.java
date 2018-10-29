5e0b760e4624aec62fd0fbeb1e9bd84269bd682d
baumannalexj <baumannalexj@gmail.com>
2018-10-28 22:24:40 -0500

    a8-hamming-numbers don't know if this matched the diagram...

--- src/alexbaumann/hammingnumbers/MainPrint.java
+++ src/alexbaumann/hammingnumbers/MainPrint.java
@@ -1,5 +1,6 @@
 package alexbaumann.hammingnumbers;
 
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentLinkedQueue;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
@@ -7,29 +8,27 @@ import java.util.concurrent.Executors;
 
 public class MainPrint {
 
-    //TODO could make more concurrent with hashmap
-    // numberTo2Exponent {  (1,0), (2,1), (3,0) ... (40, 3) }
-    // numberTo3Exponent {  (1,0), (2,0), (3,1) ... (40, 0) }
-    // numberTo5Exponent {  (1,0), (2,0), (3,0) ... (40, 1) }
-
-    private static ConcurrentLinkedQueue<String> resultQueue = new ConcurrentLinkedQueue<>();
-
     static ExecutorService executor = Executors.newFixedThreadPool(100);
+    private static ConcurrentLinkedQueue<String> resultQueue = new ConcurrentLinkedQueue<>();
 
     public static void main(String[] args) {
 
         int targetHammingNumber = 40;
 
-        CountDownLatch latch = new CountDownLatch(1);
+        CountDownLatch latch = new CountDownLatch(targetHammingNumber);
 
         OutCopy outCopy = new OutCopy(targetHammingNumber, latch, resultQueue);
 
         executor.submit(outCopy);
 
-        int startNumber = 1;
-        while (latch.getCount() != 0) {
+        int currentNumber = 1;
+        while (currentNumber <= targetHammingNumber) {
             if (resultQueue.peek() != null) {
                 System.out.println(resultQueue.poll());
+
+                currentNumber++;
+            } else if (latch.getCount() == 0) {
+                break;
             }
         }
 
@@ -38,10 +37,6 @@ public class MainPrint {
 }
 
 class OutCopy implements Runnable {
-
-    private ConcurrentLinkedQueue<Integer> divisorIsMultiple2 = new ConcurrentLinkedQueue<>();
-    private ConcurrentLinkedQueue<Integer> divisorIsMultiple3 = new ConcurrentLinkedQueue<>();
-    private ConcurrentLinkedQueue<Integer> divisorIsMultiple5 = new ConcurrentLinkedQueue<>();
 
     private int targetHammingNumber;
     private CountDownLatch parentLatch;
@@ -56,39 +51,86 @@ class OutCopy implements Runnable {
 
     @Override
     public void run() {
+
+        ConcurrentHashMap<Integer, Integer> divisorIsMultiple2 = new ConcurrentHashMap<>();
+        ConcurrentHashMap<Integer, Integer> divisorIsMultiple3 = new ConcurrentHashMap<>();
+        ConcurrentHashMap<Integer, Integer> divisorIsMultiple5 = new ConcurrentHashMap<>();
+
+        Runnable resultMerger = new InOrderMerge(
+                divisorIsMultiple2,
+                divisorIsMultiple3,
+                divisorIsMultiple5,
+                parentLatch,
+                resultQueue,
+                targetHammingNumber);
+
+        MainPrint.executor.submit(resultMerger);
+
         int currentNumber = 1;
 
         while (currentNumber <= targetHammingNumber) {
 
-            CountDownLatch latch = new CountDownLatch(3);
-            MultipleOfChecker multipleOf2 = new MultipleOfChecker(2, currentNumber, divisorIsMultiple2, latch);
-            MultipleOfChecker multipleOf3 = new MultipleOfChecker(3, currentNumber, divisorIsMultiple3, latch);
-            MultipleOfChecker multipleOf5 = new MultipleOfChecker(5, currentNumber, divisorIsMultiple5, latch);
+            MultipleOfChecker multipleOf2 = new MultipleOfChecker(2, currentNumber, divisorIsMultiple2);
+            MultipleOfChecker multipleOf3 = new MultipleOfChecker(3, currentNumber, divisorIsMultiple3);
+            MultipleOfChecker multipleOf5 = new MultipleOfChecker(5, currentNumber, divisorIsMultiple5);
 
             MainPrint.executor.submit(multipleOf2);
             MainPrint.executor.submit(multipleOf3);
             MainPrint.executor.submit(multipleOf5);
 
-            try {
-                latch.await();
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-
-            Integer exponent2 = divisorIsMultiple2.poll();
-            Integer exponent3 = divisorIsMultiple3.poll();
-            Integer exponent5 = divisorIsMultiple5.poll();
-
-            if (currentNumber == Math.pow(2, exponent2) * Math.pow(3, exponent3) * Math.pow(5, exponent5)) {
-                resultQueue.add(String.format("%d \t: 2^%d 3^%d 5^%d", currentNumber, exponent2, exponent3, exponent5));
-            }
-
             currentNumber++;
         }
 
-        parentLatch.countDown();
     }
 
+    private class InOrderMerge implements Runnable {
+        ConcurrentHashMap<Integer, Integer> divisorIsMultiple2;
+        ConcurrentHashMap<Integer, Integer> divisorIsMultiple3;
+        ConcurrentHashMap<Integer, Integer> divisorIsMultiple5;
+        private CountDownLatch parentLatch;
+        private ConcurrentLinkedQueue<String> resultQueue;
+        private int targetNumber;
+
+        public InOrderMerge(ConcurrentHashMap<Integer, Integer> divisorIsMultiple2,
+                            ConcurrentHashMap<Integer, Integer> divisorIsMultiple3,
+                            ConcurrentHashMap<Integer, Integer> divisorIsMultiple5,
+                            CountDownLatch parentLatch,
+                            ConcurrentLinkedQueue<String> resultQueue,
+                            int targetNumber) {
+            this.divisorIsMultiple2 = divisorIsMultiple2;
+            this.divisorIsMultiple3 = divisorIsMultiple3;
+            this.divisorIsMultiple5 = divisorIsMultiple5;
+            this.parentLatch = parentLatch;
+            this.resultQueue = resultQueue;
+            this.targetNumber = targetNumber;
+        }
+
+        @Override
+        public void run() {
+
+            int currentNumber = 1;
+            while (currentNumber <= targetNumber) {
+
+                Integer exponent2 = divisorIsMultiple2.get(currentNumber);
+                Integer exponent3 = divisorIsMultiple3.get(currentNumber);
+                Integer exponent5 = divisorIsMultiple5.get(currentNumber);
+
+                if (exponent2 != null
+                        && exponent3 != null
+                        && exponent5 != null) {
+
+                    if (currentNumber ==
+                            Math.pow(2, exponent2) * Math.pow(3, exponent3) * Math.pow(5, exponent5)) {
+                        resultQueue.add(String.format("%d \t: 2^%d 3^%d 5^%d", currentNumber, exponent2, exponent3, exponent5));
+
+                    }
+                    parentLatch.countDown();
+                    currentNumber++;
+
+                }
+            }
+        }
+    }
 }
 
 
@@ -96,14 +138,12 @@ class MultipleOfChecker implements Runnable {
 
     private int divisor; // 2, 3, 5
     private int potentialHammingNumber;
-    private ConcurrentLinkedQueue<Integer> resultQueue;
-    private CountDownLatch parentLatch;
+    private ConcurrentHashMap<Integer, Integer> numberToExponent;
 
-    public MultipleOfChecker(int divisor, int potentialHammingNumber, ConcurrentLinkedQueue<Integer> resultQueue, CountDownLatch parentLatch) {
+    public MultipleOfChecker(int divisor, int potentialHammingNumber, ConcurrentHashMap<Integer, Integer> numberToExponent) {
         this.divisor = divisor;
         this.potentialHammingNumber = potentialHammingNumber;
-        this.resultQueue = resultQueue;
-        this.parentLatch = parentLatch;
+        this.numberToExponent = numberToExponent;
     }
 
 
@@ -118,8 +158,7 @@ class MultipleOfChecker implements Runnable {
             exponent++;
         }
 
-        resultQueue.add(exponent);
-        parentLatch.countDown();
+        numberToExponent.put(potentialHammingNumber, exponent);
 
         //TODO do I need "interrupt = true" ??
     }
